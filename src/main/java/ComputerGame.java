167d013e5deee29a91b0e140c1c028ec9b129345
cesarion_pshebytski <yur6996@ukr.net>
2017-11-13 22:42:39 +0200

    commit

--- src/main/java/ComputerGame.java
+++ src/main/java/ComputerGame.java
@@ -7,7 +7,7 @@ class ComputerGame {
     ComputerGame(GameSpecs specs) {
         this.gameSpecs = specs;
         this.id = idCounter;
-        ComputerGame.idCounter++;
+        idCounter++;
     }
 
     int getId() {
